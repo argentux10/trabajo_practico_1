ad771592214dac01390a3c2c0992f50742add78f
Ezequiel-PC <Ezequiel-PC@DESKTOP-SELTJ5M>
2024-04-05 20:22:54 -0300

    Se creo todos los paquetes para los 10 ejercicios del tp1 y se resolvio el ejercicio 1

---
+++ tp1_JaljalEzequiel/src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -0,0 +1,11 @@
+package ar.edu.unju.fi.ejercicio1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int num1= 4, num2= 7, num3= 1, num4= 9, num5= 6;
+		float promedio = (num1+num2+num3+num4+num5)/5;
+		System.out.println("El promedio es: "+promedio);
+	}
+
+}
---
+++ tp1_JaljalEzequiel/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Ezequiel-PC
+ *
+ */
+module tp1_JaljalEzequiel {
+}
