bf681979b78e30ab235cce72d4b1ad401a03d235
Ezequiel-PC <Ezequiel-PC@DESKTOP-SELTJ5M>
2024-04-05 21:01:47 -0300

    Se resolvio el ejercicio 2 del tp1

---
+++ tp1_JaljalEzequiel/src/ar/edu/unju/fi/ejercicio2/Main.java
@@ -0,0 +1,28 @@
+package ar.edu.unju.fi.ejercicio2;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		String pais;
+		byte edad;
+		float precio;
+		double coseno;
+		int telefono, alturaEdificio;
+	
+		pais="Estados Unidos";
+		edad=23;
+		alturaEdificio=208;
+		precio=2700;
+		telefono=1122334455;
+		coseno=0.87758256189037;
+		
+		System.out.println("Pais: "+pais);
+		System.out.println("Edad: "+edad);
+		System.out.println("Altura del edificio Four Season: "+alturaEdificio);
+		System.out.println("Precio del dulce de leche: "+precio);
+		System.out.println("Numero de telefono: "+telefono);
+		System.out.println("El coseno de 0.5 es: "+coseno);
+	}
+
+}
